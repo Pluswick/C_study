91e3bb43c003ae248090718efd7b08b06c120781
DESKTOP-3GOTUVS\tjdgk <tjdgks0814@gmail.com>
2021-06-06 22:45:39 +0900

    study-6.c
    Tower of Hanoi (Baekjoon 11729 in C)

---
+++ study-6.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+void hanoi(int n, int from, int by, int to) {
+	if (n == 1)
+		printf("%d %d\n", from, to);
+	else {
+		hanoi(n - 1, from, to, by);
+		printf("%d %d\n", from, to);
+		hanoi(n - 1, by, from, to);
+	}
+}
+
+int main() {
+	int N;
+	int K, A, B;
+
+	scanf("%d", &N);
+
+	K = pow(2, N) - 1;
+	printf("%d\n", K);
+
+	hanoi(N, 1, 2, 3);
+
+	return 0;
+}
